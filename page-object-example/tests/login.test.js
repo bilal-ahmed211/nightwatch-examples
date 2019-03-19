9eab0de01051b1e83a5cfebe4a1218270e0548f2
Bilal H <bilal.h@datum.pk>
2019-03-20 01:51:27 +0500

    Changes

---
+++ page-object-example/tests/login.test.js
@@ -0,0 +1,29 @@
+var data = require('../data/login')
+module.exports = {
+  before: function (browser) {
+    var ems = browser.page.loginPage();
+    ems.navigate();
+  },
+  'Simple Login Test': function (browser) {
+    var login = browser.page.loginPage();
+    login.checkLoginWithBlankCredentials();
+  },
+
+  'Try Login with Valid Email and Blank Password': function (browser) {
+    var login = browser.page.loginPage();
+    login.checkWithValidEmailAndBlankPassword('admin@admin', '');
+
+  },
+  'Test Login with Invalid Email and Valid Password': function (browser) {
+    var login = browser.page.loginPage();
+    login.checkWithInvalidEmailAndValidPassword(data.username, data.password)
+  },
+  'Try login with valid credentials': function (browser) {
+    var login = browser.page.loginPage();
+    login.navigate()
+      .emsLoginCheck(data.username, data.password);
+  },
+
+
+
+};
