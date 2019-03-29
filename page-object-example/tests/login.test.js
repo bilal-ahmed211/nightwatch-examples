066a6100469bab506843868e8f01ed85ef71c6af
BilalAhmed211 <bilal.h@allshoreresources.com>
2019-03-29 21:04:59 +0500

    changes

--- page-object-example/tests/login.test.js
+++ page-object-example/tests/login.test.js
@@ -26,6 +26,7 @@ module.exports = {
     login.navigate()
       .emsLoginCheck(data.login.username, data.login.password);
   },
+  
    'Try login with valid credentials': function (browser) {
      var login = browser.page.loginPage();
      var data = browser.globals;
